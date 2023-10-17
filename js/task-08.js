27ff888a45f7c5f4ca8603b38553dedb0dd15e87
Aleksey-Dr <aprel181818@gmail.com>
2023-10-17 13:31:18 +0300

    Solution of task#8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const logInForm = document.querySelector('.login-form');
 
+const handleSubmit = evt => {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === '') {
+        alert('Please fill in email the field!');
+        return;
+    } else if (password === '') {
+        alert('Please fill in password the field!');
+        return;
+    }
+
+    const formFields = {
+        email,
+        password,
+    };
+
+    console.log(formFields);
+    
+    form.reset();
+};
+
+logInForm.addEventListener('submit', handleSubmit);
