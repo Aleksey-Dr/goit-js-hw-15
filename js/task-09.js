77f570b123a9c0ab56379dfacdb58f28f08bc905
Aleksey-Dr <aprel181818@gmail.com>
2023-10-17 14:11:24 +0300

    Solution of task#9

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+// console.log(getRandomHexColor());
+
+const changeColor = document.querySelector('.change-color');
+
+const handlerBtn = () => {
+  document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  document.querySelector('.color').textContent = getRandomHexColor();
+};
+
+changeColor.addEventListener('click', handlerBtn);
